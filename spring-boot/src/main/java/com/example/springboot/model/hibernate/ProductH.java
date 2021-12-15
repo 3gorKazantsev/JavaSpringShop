3e146a05a25f3a3db95594cb2b9347de66823609
Kazantsev Egor <3gork4z4ncev@gmail.com>
2021-12-15 17:06:24 +0500

    remove @Subset from ProductH entity

--- spring-boot/src/main/java/com/example/springboot/model/hibernate/ProductH.java
+++ spring-boot/src/main/java/com/example/springboot/model/hibernate/ProductH.java
@@ -3,7 +3,6 @@ package com.example.springboot.model.hibernate;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Subselect;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,11 +12,6 @@ import javax.persistence.Table;
 @Entity
 @Data
 @Immutable
-@Subselect("""
-    SELECT id, name, category_id, size FROM clothing
-    UNION 
-    SELECT id, name, category_id, size::varchar FROM footwear
-""")
 @Table(name = "product")
 public class ProductH extends ProductBaseH {
 
